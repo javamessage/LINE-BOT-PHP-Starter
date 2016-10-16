5bf709ea7c116f4277851e2bff86787bbebeff8c
Aikawa Kazuhiko <javamessage@hotmail.com>
2016-10-16 07:23:09 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -18,22 +18,15 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+//			$messages = [
+//				'type' => 'text',
+//				'text' => $text
+//			];
+
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'T_T'
 			];
-
-
-//			$messages = [
-//				{
-//					'type' => 'text',
-//					'text' => $text
-//				},
-//				{
-//					'type' => 'text',
-//					'text' => $text
-//				},
-//			];
 
 
 
@@ -41,7 +34,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages,$messages,$messages],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
